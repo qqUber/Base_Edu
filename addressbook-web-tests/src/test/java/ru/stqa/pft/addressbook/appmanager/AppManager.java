46b4017456fa417a9ced26a771df609187c816e6
qqUber <90068293+qqUber@users.noreply.github.com>
2021-10-21 23:33:18 +0300

    Refactoring lesson 2.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/AppManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/AppManager.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -40,22 +39,14 @@ public class AppManager {
     }
 
     private boolean isElementPresent(By by) {
-          try {
-              wd.findElement(by);
-              return true;
-          } catch (NoSuchElementException e) {
-              return false;
-          }
-      }
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
 
-    private boolean isAlertPresent() {
-          try {
-              wd.switchTo().alert();
-              return true;
-          } catch (NoAlertPresentException e) {
-              return false;
-          }
-      }
 
     public GroupHelper getGroupHelper() {
         return groupHelper;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavHelper.java
@@ -3,13 +3,12 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavHelper {
-   private WebDriver wd;
+public class NavHelper extends HelperBase {
     public NavHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void gotoGroupPage() {
-      wd.findElement(By.linkText("groups")).click();
+      click(By.linkText("groups"));
     }
 }
