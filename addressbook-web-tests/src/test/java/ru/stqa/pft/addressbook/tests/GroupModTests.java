1b5d262ce631a5c13f7204eb142618e22f50c16f
qqUber <90068293+qqUber@users.noreply.github.com>
2021-11-14 21:25:56 +0300

    Lesson 4.7 Successful mod&del group tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModTests extends TestBase {
@@ -16,10 +17,15 @@ public class GroupModTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupMod();
-        app.getGroupHelper().fillGroupForm(new GroupData("3", "4", "333"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "3", "4", "333");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().updateGroup();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
