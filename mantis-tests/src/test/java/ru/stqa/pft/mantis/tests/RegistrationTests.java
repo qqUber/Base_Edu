6bf0bca5bbc8c897d4cf5586e39b5f109a59ec44
qqUber <90068293+qqUber@users.noreply.github.com>
2021-12-03 12:26:03 +0300

    exercise 18

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,10 +1,39 @@
 package ru.stqa.pft.mantis.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
-    @Test
-    public void testRegistrion(){
-        app.registration().start("user1", "user1@localhost.localadmin");
-    }
+
+  //@BeforeMethod
+  public void startMailServer() {
+    app.mail().start();
+  }
+
+  @Test
+  public void testRegistration() throws IOException, MessagingException {
+    long now = System.currentTimeMillis();
+    String user = String.format("user%s", now);
+    String password = "password";
+    String email = String.format("user%s@localhost", now);
+    app.james().createUser(user, password);
+    app.registration().start(user, email);
+    //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage>mailMessages = app.james().waitForMail(user, password, 10000);
+    String confirmationLink = app.mail().findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, password);
+    assertTrue(app.httpSession().login(user, password));
+  }
+
+
+  //@AfterMethod(alwaysRun = true)
+  public void stopMailServer() {
+    app.mail().stop();
+  }
 }
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -8,5 +8,3 @@ $g_signup_use_captcha = OFF;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
 $g_enable_email_notification = ON;
-$g_default_timezone = 'Europe/Berlin';
-$g_crypto_master_salt = 'au32TQTCEAghJVSTZZdWDxwuC49YpwmB3j7wsiEEES0=';
