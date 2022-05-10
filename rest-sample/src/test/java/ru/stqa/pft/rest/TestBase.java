0c39e1d94536384cdae4631e8037096f93300dc7
qqUber <2569GitHub9652>
2022-05-10 18:41:46 +0300

    Exercise 20

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/TestBase.java
@@ -0,0 +1,34 @@
+package ru.stqa.pft.rest;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.jayway.restassured.RestAssured;
+import org.testng.SkipException;
+import org.testng.annotations.BeforeClass;
+
+import java.io.IOException;
+
+public class TestBase {
+
+    @BeforeClass
+    public void init() {
+        RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
+
+    public boolean isIssueOpen(int issueId) throws IOException {
+        return !status(issueId).equals("Resolved");
+    }
+
+    public String status(int issue_id) {
+        String json = RestAssured.get(String.format("https://bugify.stqa.ru/api/issues/" + issue_id + ".json")).asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        return parsed.getAsJsonObject().get("issues").getAsJsonArray().get(0).getAsJsonObject().get("state_name").getAsString();
+    }
+
+    public void skipIfNotFixed(int issueId) throws IOException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+}
